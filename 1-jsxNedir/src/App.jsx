faafb9a93b62b0a7dbdccdc73cdc41cc37069ea9
Recep <recepcengiz_96@outlook.com>
2024-05-08 15:41:55 +0300

    jsx nedir konusu işlendi

---
+++ 1-jsxNedir/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+/*
+  jsx js kodları içerisine html kodları yazmamızı sağlar.  JavaScript için bir syntax uzantısıdır.
+  JSX, React elementleri üretir. 
+
+  JSX, JavaScript içindeki HTML benzeri yapıları yazmayı kolaylaştırır ve bu yapıları JavaScript kodu içine entegre etmeyi sağlar.
+   Bu sayede, kullanıcı arayüzü kodları daha okunabilir ve daha kolay yönetilebilir hale gelir.
+
+*/
+ 
+  return (
+    <>
+      <div>
+        Hayat zannettiğin halat, yalnız bulunduğun dakikadır.
+      </div>
+    </>
+  )
+}
+
+export default App
