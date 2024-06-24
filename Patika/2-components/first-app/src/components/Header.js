b102dba95e44fb4f3b89916b8689dc630f068cc3
Recep <recepcengiz_96@outlook.com>
2024-06-24 09:16:39 +0300

    proptype ve props kavramı işlendi

--- Patika/2-components/first-app/src/components/Header.js
+++ Patika/2-components/first-app/src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
-export default function Header() {
+function Header({titleName,desc,isLoggedIn}) {
   return (
     <div>
       <h1>Header</h1> 
     </div>
   )
 }
+
+Header.prototype= {
+  titleName: PropTypes.string.isRequired, //gelen props ların tiplerini belirlemeye yarıyor proptypes, isRequired ile de bu propun zorunlu olduğunu belirtmiş oluyoruz.
+  desc: PropTypes.string,
+  isLoggedIn: PropTypes.bool
+}
+
+export default Header;
