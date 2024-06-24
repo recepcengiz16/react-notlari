520b82dc1480a6b1cb3a4060123e12d6ab0f7b75
Recep <recepcengiz_96@outlook.com>
2024-06-24 10:06:20 +0300

    proptype değerlerine devam edildi

--- Patika/2-components/first-app/src/components/Header.js
+++ Patika/2-components/first-app/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from "prop-types"
 
-function Header({titleName,desc,isLoggedIn}) {
+function Header({titleName,desc,isLoggedIn,age,address}) {
   return (
     <div>
       <h1>Header</h1> 
@@ -12,7 +12,12 @@ function Header({titleName,desc,isLoggedIn}) {
 Header.prototype= {
   titleName: PropTypes.string.isRequired, //gelen props ların tiplerini belirlemeye yarıyor proptypes, isRequired ile de bu propun zorunlu olduğunu belirtmiş oluyoruz.
   desc: PropTypes.string,
-  isLoggedIn: PropTypes.bool
+  age:PropTypes.oneOfType([PropTypes.number,PropTypes.string]), //bir prop birden fazla tipi kabul ederse oneOfType ile yazarız içine array alır.
+  isLoggedIn: PropTypes.bool,
+  address: PropTypes.shape({ // gelen props obje ise shape ile tip değerlerini belirleriz. İçine obje ile o propertdeki değerleri belirleriz
+    city: PropTypes.string,
+    zip: PropTypes.number
+  })
 }
 
 export default Header;
